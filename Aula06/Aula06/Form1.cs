3b7190875d519a39ea45d495d9be20627f6b185d
Alex Lopes <alex.lopes@2morrowsports.com.br>
2025-09-12 19:43:17 -0300

    first commit

---
+++ Aula06/Aula06/Form1.cs
@@ -0,0 +1,10 @@
+namespace Aula06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Aula06/Aula06/Repositorios/ContatoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06.Repositorios
+{
+    public class ContatoRepositorio
+    {
+
+        public void InserirContato()
+        {
+
+
+
+
+        }
+
+    }
+}
